9678beb8453a24b7a24839687ecdf69ccf31a96b
Matteo Ghetti <mrghettone@gmail.com>
2026-03-30 21:30:56 +0000

    trasform in vue

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { inject } from '@vercel/analytics';
+
+inject();
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
